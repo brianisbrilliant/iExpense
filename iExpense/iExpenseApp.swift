087ca5b6e7ee62d13007a408635a17053faeeee2
Brian Foster <bfoster6@butlercc.edu>
2021-04-07 08:29:16 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Professor Foster on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
