967ec302b266300c862204ebe3c15d4492cd0b41
Brian Foster <bfoster6@butlercc.edu>
2021-04-07 10:00:06 -0500

    Initial Commit

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello \(name)!")
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
             .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+        }
+        
+    }
+}
+
+//class User : ObservableObject {
+//    @Published var firstName = "Bilbo"
+//    @Published var lastName = "of Bag End"
+//}
+
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
+struct ContentView: View {
+    @State private var user = User(firstName: "Darth", lastName: "Vader")
+    
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
